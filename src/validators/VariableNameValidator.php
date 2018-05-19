5ff7bebdbe9f2ee5a85c3fa99fc19bcc1ea9051c
Tõnis Ormisson <tonis@andmemasin.eu>
2018-05-19 14:42:54 +0300

    DEV: tests

--- src/validators/VariableNameValidator.php
+++ src/validators/VariableNameValidator.php
@@ -2,15 +2,39 @@
 
 namespace dameter\abstracts\validators;
 
-use Yii;
+use yii\validators\StringValidator;
 
 
 /**
  * Class VariableNameValidator
  * @author Tõnis Ormisson <tonis@andmemasin.eu>
  */
-class VariableNameValidator extends \yii\validators\StringValidator
+class VariableNameValidator extends StringValidator
 {
     public $max = 64;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        $this->addError($model, $attribute, 'Value must not contain spaces');
+        if (strpos($model->$attribute, ' ') !== false) {
+            $this->addError($model, $attribute, 'Value must not contain spaces');
+        }
+    }
+
+    protected function validateValue($value)
+    {
+        $validation = parent::validateValue($value);
+        if (!$validation) {
+            if (strpos($value, ' ') !== false) {
+                return ['Value must not contain spaces', ['noSpaces']];
+            }
+            return null;
+        }
+        return $validation;
+    }
+
 
 }
