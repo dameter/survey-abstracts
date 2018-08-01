7763a5b792557bd5486a74b11eabf38e851072a7
Tõnis Ormisson <tonis@andmemasin.eu>
2018-08-01 08:15:54 +0300

    DEV: tests: Unit models/ConditionTest

---
+++ tests/unit/models/ConditionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace dameter\abstracts\tests\unit\models;
+
+require_once __DIR__ . "/TestBase.php";
+
+use common\modules\abstracts\tests\unit\models\TestBase;
+use dameter\abstracts\models\AnswerText;
+use dameter\abstracts\models\BaseQuestionGroup;
+use dameter\abstracts\models\Condition;
+use yii\db\ActiveQuery;
+
+class ConditionTest extends TestBase
+{
+
+    /** @var Condition */
+    protected $model;
+
+    protected $modelClass = Condition::class;
+
+    public function baseModelAttributes()
+    {
+        return [
+            'condition_id' => 1,
+            'name' => 'fake',
+            'rules' => 'fake',
+            'survey_id' => 1,
+        ];
+    }
+
+}
